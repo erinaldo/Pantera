03bf72f6d196d34e9151012943487897996c8907
juniorcrisostomo <36079970+juniorcrisostomo@users.noreply.github.com>
2018-02-26 14:47:04 -0500

    Revert "diseño"
    This reverts commit 85f6a945d80238aa316aa5bc4c46955bbf0a9480.

--- PanteraCRM/Presentacion/Formularios/frmManProductoPrincipal.designer.cs
+++ PanteraCRM/Presentacion/Formularios/frmManProductoPrincipal.designer.cs
@@ -38,9 +38,6 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.dvgProducto = new System.Windows.Forms.DataGridView();
-            this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.txtParametro = new System.Windows.Forms.TextBox();
             this.IDPRODUCTO = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.IDCHECK = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CODPRODUCTO = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -67,6 +64,9 @@
             this.NUPRECIO = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.IDSITUACION = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.CHSITUACION = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtParametro = new System.Windows.Forms.TextBox();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dvgProducto)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -76,9 +76,9 @@
             // 
             this.button1.BackColor = System.Drawing.SystemColors.Window;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(265, 14);
+            this.button1.Location = new System.Drawing.Point(268, 17);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 28);
+            this.button1.Size = new System.Drawing.Size(73, 35);
             this.button1.TabIndex = 13;
             this.button1.Text = "&Imprimir";
             this.button1.UseVisualStyleBackColor = false;
@@ -88,9 +88,9 @@
             this.btnSalir.BackColor = System.Drawing.SystemColors.Window;
             this.btnSalir.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnSalir.Location = new System.Drawing.Point(349, 14);
+            this.btnSalir.Location = new System.Drawing.Point(363, 17);
             this.btnSalir.Name = "btnSalir";
-            this.btnSalir.Size = new System.Drawing.Size(80, 28);
+            this.btnSalir.Size = new System.Drawing.Size(73, 35);
             this.btnSalir.TabIndex = 11;
             this.btnSalir.Text = "&Salir";
             this.btnSalir.UseVisualStyleBackColor = false;
@@ -100,9 +100,9 @@
             // 
             this.btnVer.BackColor = System.Drawing.SystemColors.Window;
             this.btnVer.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnVer.Location = new System.Drawing.Point(178, 14);
+            this.btnVer.Location = new System.Drawing.Point(181, 17);
             this.btnVer.Name = "btnVer";
-            this.btnVer.Size = new System.Drawing.Size(80, 28);
+            this.btnVer.Size = new System.Drawing.Size(73, 35);
             this.btnVer.TabIndex = 10;
             this.btnVer.Text = "&Ver";
             this.btnVer.UseVisualStyleBackColor = false;
@@ -112,9 +112,9 @@
             // 
             this.btnModificar.BackColor = System.Drawing.SystemColors.Window;
             this.btnModificar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnModificar.Location = new System.Drawing.Point(93, 14);
+            this.btnModificar.Location = new System.Drawing.Point(96, 17);
             this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(80, 28);
+            this.btnModificar.Size = new System.Drawing.Size(73, 35);
             this.btnModificar.TabIndex = 9;
             this.btnModificar.Text = "&Modificar";
             this.btnModificar.UseVisualStyleBackColor = false;
@@ -125,9 +125,9 @@
             this.btnAnadir.BackColor = System.Drawing.SystemColors.Window;
             this.btnAnadir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAnadir.ImageList = this.imageList1;
-            this.btnAnadir.Location = new System.Drawing.Point(6, 14);
+            this.btnAnadir.Location = new System.Drawing.Point(9, 17);
             this.btnAnadir.Name = "btnAnadir";
-            this.btnAnadir.Size = new System.Drawing.Size(80, 28);
+            this.btnAnadir.Size = new System.Drawing.Size(73, 35);
             this.btnAnadir.TabIndex = 8;
             this.btnAnadir.Text = "&Añadir";
             this.btnAnadir.UseVisualStyleBackColor = false;
@@ -148,9 +148,9 @@
             this.groupBox1.Controls.Add(this.btnSalir);
             this.groupBox1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.Black;
-            this.groupBox1.Location = new System.Drawing.Point(165, 453);
+            this.groupBox1.Location = new System.Drawing.Point(106, 400);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(434, 49);
+            this.groupBox1.Size = new System.Drawing.Size(451, 66);
             this.groupBox1.TabIndex = 14;
             this.groupBox1.TabStop = false;
             // 
@@ -187,41 +187,13 @@
             this.NUPRECIO,
             this.IDSITUACION,
             this.CHSITUACION});
-            this.dvgProducto.Location = new System.Drawing.Point(12, 60);
+            this.dvgProducto.Location = new System.Drawing.Point(12, 49);
             this.dvgProducto.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dvgProducto.Name = "dvgProducto";
             this.dvgProducto.ReadOnly = true;
             this.dvgProducto.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            this.dvgProducto.Size = new System.Drawing.Size(742, 392);
+            this.dvgProducto.Size = new System.Drawing.Size(647, 346);
             this.dvgProducto.TabIndex = 15;
-            // 
-            // groupBox2
-            // 
-            this.groupBox2.Controls.Add(this.label1);
-            this.groupBox2.Controls.Add(this.txtParametro);
-            this.groupBox2.Location = new System.Drawing.Point(12, 5);
-            this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(742, 48);
-            this.groupBox2.TabIndex = 16;
-            this.groupBox2.TabStop = false;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(55, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Búsqueda";
-            // 
-            // txtParametro
-            // 
-            this.txtParametro.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
-            this.txtParametro.Location = new System.Drawing.Point(70, 17);
-            this.txtParametro.Name = "txtParametro";
-            this.txtParametro.Size = new System.Drawing.Size(561, 20);
-            this.txtParametro.TabIndex = 0;
-            this.txtParametro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtParametro_KeyPress);
             // 
             // IDPRODUCTO
             // 
@@ -245,7 +217,7 @@
             this.CODPRODUCTO.HeaderText = "CODIGO";
             this.CODPRODUCTO.Name = "CODPRODUCTO";
             this.CODPRODUCTO.ReadOnly = true;
-            this.CODPRODUCTO.Width = 80;
+            this.CODPRODUCTO.Width = 60;
             // 
             // CHFAMILIA
             // 
@@ -253,6 +225,7 @@
             this.CHFAMILIA.HeaderText = "CATEGORIA";
             this.CHFAMILIA.Name = "CHFAMILIA";
             this.CHFAMILIA.ReadOnly = true;
+            this.CHFAMILIA.Width = 90;
             // 
             // CHMARCA
             // 
@@ -428,6 +401,34 @@
             this.CHSITUACION.Name = "CHSITUACION";
             this.CHSITUACION.ReadOnly = true;
             // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtParametro);
+            this.groupBox2.Location = new System.Drawing.Point(12, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(647, 43);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Busqueda";
+            // 
+            // txtParametro
+            // 
+            this.txtParametro.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtParametro.Location = new System.Drawing.Point(67, 15);
+            this.txtParametro.Name = "txtParametro";
+            this.txtParametro.Size = new System.Drawing.Size(574, 20);
+            this.txtParametro.TabIndex = 0;
+            this.txtParametro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtParametro_KeyPress);
+            // 
             // frmManProductoPrincipal
             // 
             this.AcceptButton = this.btnAnadir;
@@ -436,7 +437,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.CancelButton = this.btnSalir;
-            this.ClientSize = new System.Drawing.Size(785, 513);
+            this.ClientSize = new System.Drawing.Size(671, 478);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.dvgProducto);
             this.Controls.Add(this.groupBox1);
@@ -465,9 +466,6 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.DataGridView dvgProducto;
-        private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtParametro;
         private System.Windows.Forms.DataGridViewTextBoxColumn IDPRODUCTO;
         private System.Windows.Forms.DataGridViewTextBoxColumn IDCHECK;
         private System.Windows.Forms.DataGridViewTextBoxColumn CODPRODUCTO;
@@ -494,5 +492,8 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn NUPRECIO;
         private System.Windows.Forms.DataGridViewTextBoxColumn IDSITUACION;
         private System.Windows.Forms.DataGridViewTextBoxColumn CHSITUACION;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtParametro;
     }
 }
